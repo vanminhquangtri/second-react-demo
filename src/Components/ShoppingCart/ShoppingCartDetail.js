4aa07ac6432243aa29c2ba91a822ad06aceeb684
Van Minh <vanminhquangtri@gmail.com>
2020-10-25 12:59:11 +0700

    pending format shopping-cart-detail

--- src/Components/ShoppingCart/ShoppingCartDetail.js
+++ src/Components/ShoppingCart/ShoppingCartDetail.js
@@ -26,7 +26,38 @@ const ShoppingCartDetail = (props) => {
                     <div className="col-6 order">
                         <div className="content">
                             <div className="title">Order Summary</div>
-                            <div className="info">Detail Info</div>
+                            <div className="info">
+                                <div className="row product-row">
+                                    <div className="col-2 img">
+                                        <img 
+                                            src="https://images.unsplash.com/photo-1570949144026-8f8141748760?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80"
+                                            alt="product"
+                                        />
+                                    </div>
+                                    <div className="col-10 product-detail">
+                                        <div className="name">
+                                            <NavLink
+                                                to = "/"
+                                                exact = {true}
+                                            >
+                                                Fitness Trousers For Women White
+                                            </NavLink>
+                                        </div>
+                                        <div className="price">
+                                            $210.00
+                                        </div>
+                                        <div className="quantity-title">Quantity</div>
+                                        <div className="row quantity-row">
+                                            <div className="quantity">
+                                                <input
+                                                    type="number"
+                                                    value="1"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="col-3 total-amount">

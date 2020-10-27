4bb9744db61f4cee241e238cbbd310465d83c6d2
Van Minh <vanminhquangtri@gmail.com>
2020-10-27 09:20:49 +0700

    done render city nae base on country name and done render shipping fee

--- src/Components/CheckoutShoppingCart/CheckoutContactShipping.js
+++ src/Components/CheckoutShoppingCart/CheckoutContactShipping.js
@@ -1,57 +1,75 @@
 import React, {useState} from 'react';
 import {NavLink} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faReplyAll, faLock} from "@fortawesome/free-solid-svg-icons";
+import {faReplyAll} from "@fortawesome/free-solid-svg-icons";
 
 const CheckoutContactShipping = (props) => {
     const [state, setState] = useState({
-        form_status: "contact"
+        country: ""
     })
+    const {Countries, changeShippingFee} = props;
+    const currentCountry = Countries.find((ct) => {
+        return ct.code === state.country
+    })
+    const changeCountry = (ev) => {
+        ev.preventDefault();
+        const value = ev.target.value;
+        setState((prevState) => {
+            return {
+                ...prevState,
+                country: value
+            }
+        })
+    }
     return (
         <div className="contact-shipping">
             <form>
                 <label className="form-field">Shipping Address</label>
-                <select name="country" className="field">
-                    <option>Please Choose Your Country</option>
-                    <option>UK</option>
-                    <option>USA</option>
-                    <option>France</option>
+                <select name="country" className="field" onChange = {(ev)=> {changeCountry(ev)}}>
+                    <option value="">Please Choose Your Country</option>
+                    <option value="UK">UNITED KINGDOM</option>
+                    <option value="US">UNITED STATES</option>
+                    <option value="FR">FRANCE</option>
                 </select>
-                <select name="country" className="field">
-                    <option>Please Choose Your City</option>
-                    <option>London</option>
-                    <option>Manchester</option>
-                    <option>Liverpool</option>
+                <select name="country" className="field" onChange = {(ev)=>{changeShippingFee(ev)}}>
+                    {/* render city name base on country name */}
+                    <option value="">Please Choose Your City</option>
+                    {
+                        (currentCountry !== undefined) && currentCountry.regions.map((region) => {
+                            return (
+                                <option value = {region.fee} key = {region.name}>{region.name}</option>
+                            )
+                        })
+                    }
                 </select>
                 <input className="field" name="street" type="text" placeholder="Street name: No. 20, Saint Maxim Street etc"/>
                 <input className="field" name="more" type="text" placeholder="More detail such as District, Ward etc"/>
                 <input className="field" name="phone" type="tel" placeholder="Your phone number"/>
                 <input className="field" name="email" type="email" placeholder="Email address"/>
-            </form>
-            <div className="navi">
-                <div class="container">
-                    <div class="row">
-                    <div className="col-6">
-                        <div className="wrap pre">
-                            <NavLink
-                                to = "/shopping-cart"
-                                exact = {true}
-                            >
-                                <FontAwesomeIcon icon = {faReplyAll} className="icon"/>
-                                Return to Shopping Cart
-                            </NavLink>
+                <div className="navigate">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-6">
+                                <div className="wrap pre">
+                                    <NavLink
+                                        to = "/shopping-cart"
+                                        exact = {true}
+                                    >
+                                        <FontAwesomeIcon icon = {faReplyAll} className="icon"/>
+                                        Return to Shopping Cart
+                                    </NavLink>
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div className="wrap next">
+                                    <input className="field" name="street" type="submit" value="Continue to Payment Stage"/>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-6">
-                        <div className="wrap next">
-                            <span>Continue to Payment Stage</span>
-                        </div>
-                    </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     );
 };
-
-export default CheckoutContactShipping;
+export default CheckoutContactShipping
--- src/Components/CheckoutShoppingCart/CheckoutShoppingCart.js
+++ src/Components/CheckoutShoppingCart/CheckoutShoppingCart.js
@@ -1,11 +1,117 @@
 import React, {useState} from 'react';
-import {NavLink} from "react-router-dom";
 import CheckoutContactShipping from './CheckoutContactShipping';
+import {connect} from "react-redux";
+
+const countryData = [
+    // shipping fee default USD
+    {
+        code: "UK",
+        name: "UNITED KINGDOM",
+        regions : [
+            {
+                name: "London",
+                fee: 30,
+            },
+            {
+                name: "Manchester",
+                fee: 40,
+            },
+            {
+                name: "Liverpool",
+                fee: 50
+            }
+        ]
+    },
+    {
+        code: "US",
+        name: "UNITED STATES",
+        regions : [
+            {
+                name: "Las Vegas",
+                fee: 35,
+            },
+            {
+                name: "Los Angeles",
+                fee: 45,
+            },
+            {
+                name: "New York",
+                fee: 55
+            }
+        ]
+    },
+    {
+        code: "FR",
+        name: "FRANCE",
+        regions : [
+            {
+                name: "Paris",
+                fee: 32,
+            },
+            {
+                name: "Reims",
+                fee: 42,
+            },
+            {
+                name: "Lyon",
+                fee: 52
+            }
+        ]
+    }
+]
 
 const CheckoutShoppingCart = (props) => {
+    const {Cart, Currency} = props.Data;
     const [state, setState] = useState({
-        form_status: "contact"
+        shipping_fee: 0
     })
+    // format thounds seperator
+    function formatNumber(num) {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
+    // fortmat money (currency and operator and decimal)
+    const showMoney = (currency, quantity, item) => {
+        switch (currency) {
+            case "USD":
+                return "$ " + formatNumber((item.price*quantity).toFixed(2))
+
+            case "EUR":
+                return "€ " + formatNumber((item.price*quantity * 0.84).toFixed(2))
+
+            case "GBP":
+                return "£ " + formatNumber((item.price*quantity * 0.76).toFixed(2))
+
+            default:
+                return "$ " + formatNumber((item.price*quantity).toFixed(2))
+        }
+    }
+    // fortmat money (currency and operator and decimal)
+    const showMoneyTotal = (currency, amount) => {
+        switch (currency) {
+            case "USD":
+                return "$ " + formatNumber((amount).toFixed(2))
+
+            case "EUR":
+                return "€ " + formatNumber((amount * 0.84).toFixed(2))
+
+            case "GBP":
+                return "£ " + formatNumber((amount * 0.76).toFixed(2))
+
+            default:
+                return "$ " + formatNumber((amount).toFixed(2))
+        }
+    }
+    const changeShippingFee = (ev) => {
+        ev.preventDefault();
+        let value = ev.target.value;
+        if (value === "") {value = 0};
+        setState((prevState) => {
+            return {
+                ...prevState,
+                shipping_fee: parseFloat(value)
+            }
+        })
+    }
     return (
         <section className="check-out">
             <div className="container">
@@ -17,7 +123,10 @@ const CheckoutShoppingCart = (props) => {
                 <div className="row payment">
                     <div className="col-7 payment-process">
                         <div className="content">
-                            <CheckoutContactShipping></CheckoutContactShipping>
+                            <CheckoutContactShipping
+                                Countries = {countryData}
+                                changeShippingFee = {changeShippingFee}
+                            />
                         </div>
                     </div>
                     <div className="col-5 summary">
@@ -26,21 +135,22 @@ const CheckoutShoppingCart = (props) => {
                         </div>
                         <div className="content">
                             <table>
-                                <tbody><tr className="product">
-                                    <td className="name">Latest Beauty Women Clothing White</td>
-                                    <td className="quantity">10</td>
-                                    <td className="price">$ 200.00</td>
-                                    <td className="amount">$ 2000.00</td>
+                                <tbody>
+                                    <tr className="product">
+                                        <td className="name">Latest Beauty Women Clothing White</td>
+                                        <td className="quantity">10</td>
+                                        <td className="price">$ 200.00</td>
+                                        <td className="amount">$ 2000.00</td>
                                     </tr>
                                     <tr className="product">
-                                    <td className="name">Shipping Fee</td>
-                                    <td className="quantity">1</td>
-                                    <td className="price">$ 200.00</td>
-                                    <td className="amount">$ 2000.00</td>
+                                        <td className="name">Shipping Fee</td>
+                                        <td className="quantity">1</td>
+                                        <td className="price">$ 200.00</td>
+                                        <td className="amount">{showMoneyTotal(Currency.currency,state.shipping_fee)}</td>
                                     </tr>
-                                    <tr className="total-amount">
-                                    <td className="title" colSpan={3}>Total</td>
-                                    <td className="money">$ 2000.00</td>
+                                        <tr className="total-amount">
+                                        <td className="title" colSpan={3}>Total</td>
+                                        <td className="money">$ 2000.00</td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -51,5 +161,9 @@ const CheckoutShoppingCart = (props) => {
         </section>
     );
 };
-
-export default CheckoutShoppingCart;
+const mapStateToProps = (state) => {
+    return {
+        Data: state
+    }
+}
+export default connect(mapStateToProps)(CheckoutShoppingCart)

bd4eee7ff90bf873ffae42f0fa0dceedf2240a55
Van Minh <vanminhquangtri@gmail.com>
2020-10-29 13:09:14 +0700

    check if input order-number is exist in Store when press View Order of component Order

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -34,16 +34,40 @@ const ShoppingCartProduct = (props) => {
             })
         }
     }
-    // update state email and order number when fill form
+    // update state email and order number and change state to verify when fill each input tag
     const updateEmailAndOrderNumber = (ev, field) => {
         const value = ev.target.value;
         const currentState = {...state};
         currentState[field] = value;
+        currentState.stage = "verify"
         setState(() => {
             return currentState
         })
     }
-    console.log(Orders);
+    // check if input order-number is exist in Store when press "View Order"
+    const findOrderInStore = (ev, orderNumber, passedEmail) => {
+        ev.preventDefault();
+        const result = Orders.some((order) => {
+            return order.order_id === orderNumber && order.shipping.email === passedEmail
+        });
+        // if exist, change state to success
+        if (result) {
+            setState((prevState) => {
+                return {
+                    ...prevState,
+                    stage: "success"
+                }
+            })
+        } else {
+            // if not exist, change state to failed
+            setState((prevState) => {
+                return {
+                    ...prevState,
+                    stage: "failed"
+                }
+            })
+        }
+    }
     return (
         <section className="order">
             <div className="container">
@@ -57,7 +81,7 @@ const ShoppingCartProduct = (props) => {
                         <div className="row order-verify">
                             <div className="col">
                                 <h4>Please fill all information below</h4>
-                                <form onSubmit = {(ev)=>{updateState(ev)}}>
+                                <form onSubmit = {(ev)=>{findOrderInStore(ev, state.order_number, state.email)}}>
                                     <label className="label">Order number</label>
                                     <input required className="field" type="text" placeholder="Your order number, for example: ORDER-1234567" onChange = {(ev)=>{updateEmailAndOrderNumber(ev, "order_number")}}/><br/>
                                     <label className="label">Emaill address</label>

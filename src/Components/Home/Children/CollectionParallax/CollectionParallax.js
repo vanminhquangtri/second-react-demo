6423f9c22ec5cb1317cd90aeff0a69c1f77ad6c0
Van Minh <vanminhquangtri@gmail.com>
2020-11-02 09:57:21 +0700

    remove animate.css

--- src/Components/Home/Children/CollectionParallax/CollectionParallax.js
+++ src/Components/Home/Children/CollectionParallax/CollectionParallax.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Parallax} from 'react-parallax';
 
 import {NavLink} from "react-router-dom";
--- src/Components/Home/Children/TopSlide/TopSlide.js
+++ src/Components/Home/Children/TopSlide/TopSlide.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import "animate.css";
 
 const TopSlide = (props) => {
     return (
